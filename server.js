913d23770772b0449dd33d82635c23705f180263
subbufrom <subbufrom@gmail.com>
2017-05-05 20:37:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,88 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
 
-			'article-one' : {
-			title:'article one by subbu', 	
-			heading: 'Article one',
-			date: 'feb 7 2017',
-			content:`        <p>
-								for the first article
-								This is my content for the first webpage that i have created.
-								This is my content for the first webpage that i have created and
-								being executed
-							</p>`
-            },
 
-			'article-two':{
-				title:'article two by subbu', 	
-			heading: 'Article two',
-			date: 'feb 8 2017',
-			content:`        <p>
-								for the first article
-								This is my content for the first webpage that i have created.
-								This is my content for the first webpage that i have created and
-								being executed
-							</p>`
-			},
 
-			'article-three':{
-				title:'article three by subbu', 	
-			heading: 'Article three',
-			date: 'feb 9 2017',
-			content:`        <p>
-								for the first article
-								This is my content for the first webpage that i have created.
-								This is my content for the first webpage that i have created and
-								being executed
-							</p>`
-			}
-};
 
-function createTemplate (data){
-	        var title = data.title;
-			var date = data.date;
-			var heading = data.heading;
-			var content = data.content;
-			var htmltemplate = `
-			<html>
-				<head>
-					<title>${title}</title>
-					<meta name="viewport" content="width=device-width, initial-scale=1">
-					<link href="/ui/style.css" rel="stylesheet" />
-				</head>
-				<body>
-					<div class="container">
-						<div>
-							<a href="/">HOME</a>
-						</div>
-						<hr/>
-						<h2>${heading}</h2>
-						
-						<div>
-							${date}
-						</div>
-							<div>
-								${content}
-							
-							</div>	
-					</div>
-				</body>
-			</html>
-			`;
-			return htmltemplate;
-}
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/:articleName', function(req, res) {
-	var articleName= req.params.articleName;
-	res.send(createTemplate(articles[articleName]));
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -100,17 +29,46 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/123.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '123.png'));
+});
+
+app.get('/loginsuccess.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'loginsuccess.html'));
+});
+
+
+app.get('/index.html',function(req, res){
+	
+	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/resume.html',function(req, res){
+	
+	res.sendFile(path.join(__dirname, 'ui', 'resume.html'));
+});
+
 
 app.get('/ui/w3.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'w3.css'));
 });
 
 
-
-app.get('/ui/123.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '123.png'));
+app.get('/ui/img/coding.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','img', 'coding.jpg'));
 });
 
+app.get('/img/html5.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','img', 'html5.png'));
+});
+
+app.get('/img/css3.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','img', 'css3.png'));
+});
+
+app.get('/img/nodejs.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','img', 'nodejs.png'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
