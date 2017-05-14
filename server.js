74db15cdd2172af1db79302896c7bdfa356eb0b2
subbufrom <subbufrom@gmail.com>
2017-05-14 14:02:46 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -17,17 +17,41 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/css/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','css','style.css'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+
+app.get('/js/jquery.particleground.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','js','jquery.particleground.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/js/demo.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','js','demo.js'));
 });
+
+app.get('/img/showicon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','img','showicon.png'));
+});
+
+
+
+app.get('/css/w3.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','css','w3.css'));
+});
+
+app.get('/img/123.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','img','123.png'));
+});
+
+
+app.get('/work.html',function(req, res){
+	
+	res.sendFile(path.join(__dirname, 'ui', 'work.html'));
+});
+
+
+
 
 app.get('/ui/123.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '123.png'));
